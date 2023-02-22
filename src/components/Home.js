3d9e62b0dde0cc68a17b0013cf32e5805feb8bd8
cusmoritz <cus.moritz@gmail.com>
2023-02-21 18:07:30 -0700

    First render

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import InputForm from "./InputForm";
+
+const Home = () => {
+    return (
+        <div>
+            <p>Home world.</p>
+            <InputForm />
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/components/InputForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const InputForm = () => {
+    return (
+        <div>
+            <form>
+                <label htmlFor="business">Business</label>
+                <input type="text" placeholder="Name"></input>
+            </form>
+        </div>
+    )
+};
+
+export default InputForm;
